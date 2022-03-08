764a415eb38febe3ce11d1c722afac60f8f6c450
Bidisha742 <bidishaasarkar@gmail.com>
2022-03-08 22:01:08 +0530

    my first commit

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import "./ExpenseForm.css";
+const ExpenseForm = (props) => {
+  const [eneteredTitle, setEnteredTitle] = useState("");
+  const [eneteredAmount, setEnteredAmount] = useState("");
+  const [eneteredDate, setEnteredDate] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: eneteredTitle,
+      amount: eneteredAmount,
+      date: new Date(eneteredDate),
+    };
+
+    props.onSaveExpenseData(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            onChange={titleChangeHandler}
+            value={eneteredTitle}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            onChange={amountChangeHandler}
+            min="0.01"
+            step="0.01 "
+            value={eneteredAmount}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            onChange={dateChangeHandler}
+            min="2019-01-01"
+            max="2022-12-31"
+            value={eneteredDate}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>Cancel</button>
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
